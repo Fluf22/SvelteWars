3b31bf181c694ebf18a982ad70b4065fd4cea6a0
Thomas Raffray <thomas.raffray@e.email>
2020-11-15 19:36:32 +0100

    File formatting

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Nav from '../components/Nav.svelte';
+	import Nav from "../components/Nav.svelte";
 
 	export let segment: string;
 </script>
@@ -15,8 +15,8 @@
 	}
 </style>
 
-<Nav {segment}/>
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+	<slot />
 </main>
